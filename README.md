aa8d694a90320c94eea1813f55fea43429187030
Gerrit Paepcke <gerrit.paepcke@gmail.com>
2022-03-26 16:52:45 +0100

    Updated Example & Readme

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
   ! BE CAREFUL WITH MULTIPLAYER - STILL IN TESTING
 
 # API
+Our Wiki you can find here:
+English: https://docs.settlers-united.com/v/s4-warriorslib-en
+German: https://docs.settlers-united.com/v/s4-warriorslib-de
 
 The API consists of a few functions and an enum that contains all possible types of movement
 
@@ -15,26 +18,18 @@ The enum:
     - WarriorsLib.MOVE_STOP: Stop a group of units, x,y passed to this action do not matter.
 
 The functions: 
-- WarriorsLib.SelectWarriors(x, y, r, party, settlertype)
-   - Selects all entities of type <settlertype> (e.g. Settlers.SWORDSMAN_01) of a <party> around a circle with the center of <x>/<y> and radius <r>
+- WarriorsLib.SelectWarriors(x, y, r, player, settlertype)
+   - Selects all entities of type <settlertype> (e.g. Settlers.SWORDSMAN_01) of a <player> around a circle with the center of <x>/<y> and radius <r>
    - Returns an index with a reference to the selection
 
 - WarriorsLib.Send(group, x, y, movementtype)
    - Sends a <group> of entities to the coordinate <x>/<y>. The behavior in which they walk towards this point is determined by <movementtype>
 
-- WarriorsLib.AiGarrisonWarriors(buildingID, party)
-   - Garrison a building. This makes a military building to search nearby units and when found command them to enter the specified building to completely occupy all spots in the building.
-   - ONLY works for AIs
- 
-- WarriorsLib.AiUnGarrisonWarriors(buildingID, column, bowman, party)
-   - Ungarrison a building. This makes a military building eject units
-   - ONLY works for AIs
-   - column: The column of the unit to eject. Use 0 (zero) to eject the leftmost unit (in regards to the side panel of the selected building) . Use -1 to eject as much units as possible. This value is usually in the range -1..5.
-   - bowman: If TRUE a bowman is ejected. Otherwise a swordman is ejected. This parameter is ignored if column is -1.
+- WarriorsLib.getPlayerName(player)
+   - Return the PlayerName of the given Player. 
 
-- WarriorsLib.AiRecruitWarriors(buildingID, settlertype, amount, party)
+- WarriorsLib.RecruitWarriors(buildingID, settlertype, amount, player)
    - Let a building start or stop recruiting settlers. 
-   - ONLY works for AIs
    - settlertype: for example Settlers.SWORDSMAN_01 or Settlers.BOWMAN_03
    - amount: 
 	
@@ -47,6 +42,28 @@ The functions:
 | 5     | Add five units to the queue.                                 |
 | 100   | The game will interpret this as infinity and will never decrease the queue. |
 
+
+- WarriorsLib.GarrisonWarriors(buildingID, player)
+   - Garrison a building. This makes a military building to search nearby units and when found command them to enter the specified building to completely occupy all spots in the building.
+ 
+- WarriorsLib.UnGarrisonWarriors(buildingID, column, bowman, player)
+   - Ungarrison a building. This makes a military building eject units
+   - column: The column of the unit to eject. Use 0 (zero) to eject the leftmost unit (in regards to the side panel of the selected building) . Use -1 to eject as much units as possible. This value is usually in the range -1..5.
+   - bowman: If TRUE a bowman is ejected. Otherwise a swordman is ejected. This parameter is ignored if column is -1.
+
+- WarriorsLib.SetTradingRoute(sourceBuildingID, destinationBuildingID, player)
+   - Set the target market/harbor of a market/harbor to establish a trading route.
+ 
+ - WarriorsLib.TradeGood(buildingID, goodtype, amount, player)
+   - Specifies what goods and how many a market or harbor shall transport. Use WarriorsLib.SetTradingRoute to establish the destination building for the trade.
+ 
+ - WarriorsLib.StoreGood(buildingID, goodtype, enable, player)
+   - Specifies what goods a storagearea store.
+   - enable: If TRUE the building will start to store goods of the specified type. If FALSE it will stop storing them.
+ 
+ - WarriorsLib.SetBuildingWorkarea(buildingID, x, y, player)
+   - Set the work area for a building. This is the same event that players can issue by clicking the target icon in the side panel of a selected building.
+ 
 # Tipp
 Example to check within new_game() if WarriorsLib is installed by the user
 
--- S4WarriorsLib/Example/warriortest.lua
+++ S4WarriorsLib/Example/warriortest.lua
@@ -1,55 +1,59 @@
-
-function vcc()
-	
-end
-
-function addUnits(id, x, y, amount, settlertype)
-	settlertype = settlertype or Settlers.SWORDSMAN_03
-	Settlers.AddSettlers(x, y, id, settlertype, amount)	
-end
-
 function new_game()
 	MinuteEvents.new_game()
 	dbg.stm("Spielstart! Ich heisse dich Willkommen zu dem Showcase von der S4 Script Erweiterung 'WarriorsLib'")
+	dbg.stm("---------")
 	request_event(gameloop, Events.FIVE_TICKS)
-	request_event(vcc,Events.VICTORY_CONDITION_CHECK)
-	dbg.stm("Hello "..WarriorsLib.getPlayerName(1)..", you are a ".. (WarriorsLib.isHuman(1) == 1 and "Human" or "AI"))
-	dbg.stm("Hello "..WarriorsLib.getPlayerName(2)..", you are a ".. (WarriorsLib.isHuman(2) == 1 and "Human" or "AI"))
-	dbg.stm("Hello "..WarriorsLib.getPlayerName(3)..", you are a ".. (WarriorsLib.isHuman(3) == 1 and "Human" or "AI"))
+
 	dbg.aioff(1)
 	dbg.aioff(2)
 	dbg.aioff(3)
-		
-	--WarriorsLib.RecruitWarriors(Buildings.GetFirstBuilding(1, Buildings.BARRACKS), Settlers.SWORDSMAN_03, 100)
-	b1 = Buildings.GetFirstBuilding(1, Buildings.MARKETPLACE)
-	b2 = Buildings.GetFirstBuilding(3, Buildings.MARKETPLACE)
-	WarriorsLib.SetTradingRoute(b2, b1, 3, 1)
-	WarriorsLib.TradeGood(b2, Goods.GOLDBAR, 100, 3, 1)
-	WarriorsLib.SetBuildingWorkarea(Buildings.GetFirstBuilding(3, Buildings.BARRACKS),70, 33, 3, 1)
-	--WarriorsLib.ShowMessageBox(500,500,200,50,"testheader","testnachricht")
 	
+	-- Einlagern von Gold
 	Buildings.AddBuilding(118, 58, 3, Buildings.STORAGEAREA)
 	WarriorsLib.StoreGood (Buildings.GetFirstBuilding(3, Buildings.STORAGEAREA), Goods.GOLDBAR, 1, 3, 1)
+end
+function showAi()
+	dbg.stm(WarriorsLib.getPlayerName(1)..", you are ".. (WarriorsLib.isHuman(1) == 1 and "a Human" or "an AI"))
+	dbg.stm(WarriorsLib.getPlayerName(2)..", you are ".. (WarriorsLib.isHuman(2) == 1 and "a Human" or "an AI"))
+	dbg.stm(WarriorsLib.getPlayerName(3)..", you are ".. (WarriorsLib.isHuman(3) == 1 and "a Human" or "an AI"))
+	dbg.stm("---------")
 end
 
 function register_functions()
 	reg_func(gameloop)
-	reg_func(vcc)
 	MinuteEvents.register_functions()
 end
 
 function register_minute_events()
-	requestMinuteEvent(turmBesetzen,4)
+	requestMinuteEvent(turmBesetzen,1)
+	requestMinuteEvent(setupTrading,2)
+	requestMinuteEvent(recruitBowman,3)
+	requestMinuteEvent(setWorkingAreaAndRecruit,4)
 	requestMinuteEvent(turmUnBesetzen,5)
-	requestMinuteEvent(sp1_5l3,1)
-	requestMinuteEvent(sp2_5l3,2)
-	requestMinuteEvent(sp3_20l3,3)
+end
+
+function setupTrading()
+	dbg.stm("Wir setzen eine Handelsroute mit: ")
+	dbg.stm("WarriorsLib.SetTradingRoute(SourceBuilding, DestBuilding, player)")
+	dbg.stm("WarriorsLib.SetTradingRoute(Buildings.GetFirstBuilding(3, Buildings.MARKETPLACE), Buildings.GetFirstBuilding(1, Buildings.MARKETPLACE), 3)")
+	dbg.stm("---------")
+	dbg.stm("Festlegen was verschickt werden soll: ")
+	dbg.stm("WarriorsLib.TradeGood(SourceBuilding, goodtype, amount, player)")
+	dbg.stm("WarriorsLib.TradeGood(Buildings.GetFirstBuilding(3, Buildings.MARKETPLACE), Goods.GOLDBAR, 100, 3)")
+	dbg.stm("---------")
+	
+	b1 = Buildings.GetFirstBuilding(1, Buildings.MARKETPLACE)
+	b2 = Buildings.GetFirstBuilding(3, Buildings.MARKETPLACE)
+	WarriorsLib.SetTradingRoute(b2, b1, 3)
+	WarriorsLib.TradeGood(b2, Goods.GOLDBAR, 100, 3)
 end
 
 function gameloop()
 	local x = 150
 	local y = 200
-	-- dbg.stm("1")
+	if newMinute() == 1 then
+		showAi()
+	end
 
 	addUnits(1, 85, 125, 1, Settlers.SWORDSMAN_03)
 	unit1 = WarriorsLib.SelectWarriors(85, 125, 20, 1, Settlers.SWORDSMAN_03)
@@ -58,44 +62,68 @@ function gameloop()
 	addUnits(2, 170, 125, 1, Settlers.SWORDSMAN_03)
 	unit2 = WarriorsLib.SelectWarriors(170, 125, 20, 2, Settlers.SWORDSMAN_03)
 	WarriorsLib.Send(unit2,x,y,WarriorsLib.MOVE_FORWARD)
-	
-	-- addUnits(3, 100, 70, 1, Settlers.SWORDSMAN_03)
-	-- unit3 = WarriorsLib.SelectWarriors(100, 70, 20, 3, Settlers.SWORDSMAN_03)
-	-- WarriorsLib.Send(unit3,x,y,WarriorsLib.MOVE_FORWARD)
 
 end
 
 function turmBesetzen()
-	dbg.stm("Alle Türme besetzen")
+	dbg.stm("Wir besetzen nun alle Türme")
+	dbg.stm("WarriorsLib.GarrisonWarriors(buildingID, player)")
+	dbg.stm("WarriorsLib.GarrisonWarriors(Buildings.GetFirstBuilding(1, Buildings.GUARDTOWERSMALL),1)")
+	dbg.stm("---------")
 	WarriorsLib.GarrisonWarriors(Buildings.GetFirstBuilding(1, Buildings.GUARDTOWERSMALL),1)
 	WarriorsLib.GarrisonWarriors(Buildings.GetFirstBuilding(2, Buildings.GUARDTOWERSMALL),2)
-	WarriorsLib.GarrisonWarriors(Buildings.GetFirstBuilding(3, Buildings.GUARDTOWERSMALL),3, 1)
+	WarriorsLib.GarrisonWarriors(Buildings.GetFirstBuilding(3, Buildings.GUARDTOWERSMALL),3)
 end
 
 function turmUnBesetzen()
-	dbg.stm("Alle Türme unbesetzen")
+	dbg.stm("Wir holen alle Soldaten aus den Türmen")
+	dbg.stm("WarriorsLib.UnGarrisonWarriors(buildingID, column, bowman, player)")
+	dbg.stm("WarriorsLib.UnGarrisonWarriors(Buildings.GetFirstBuilding(1, Buildings.GUARDTOWERSMALL),-1,1,1)")
+	dbg.stm("---------")
 	WarriorsLib.UnGarrisonWarriors(Buildings.GetFirstBuilding(1, Buildings.GUARDTOWERSMALL),-1,1,1)
 	WarriorsLib.UnGarrisonWarriors(Buildings.GetFirstBuilding(2, Buildings.GUARDTOWERSMALL),-1,1,2)
-	WarriorsLib.UnGarrisonWarriors(Buildings.GetFirstBuilding(3, Buildings.GUARDTOWERSMALL),-1,1,3, 1)
+	WarriorsLib.UnGarrisonWarriors(Buildings.GetFirstBuilding(3, Buildings.GUARDTOWERSMALL),-1,1,3)
+	showAi()
 end
 
-function sp1_5l3()
-	dbg.stm("SP1 5x L3 SWORDSMAN_03")
-	WarriorsLib.RecruitWarriors(Buildings.GetFirstBuilding(1, Buildings.BARRACKS), Settlers.SWORDSMAN_03, 5, 1)
-end
-function sp2_5l3()
-	dbg.stm("SP2 5x L3 BOWMAN_03")
+function recruitBowman()
+	dbg.stm("Wir rekrutieren bei allen Spielern nun 5 Bogenschützen:")
+	dbg.stm("WarriorsLib.RecruitWarriors(buildingID, settlertype , amount, player)")
+	dbg.stm("WarriorsLib.RecruitWarriors(Buildings.GetFirstBuilding(1, Buildings.BARRACKS), Settlers.BOWMAN_03, 5, 1)")
+	dbg.stm("---------")
+	WarriorsLib.RecruitWarriors(Buildings.GetFirstBuilding(1, Buildings.BARRACKS), Settlers.BOWMAN_03, 5, 1)
 	WarriorsLib.RecruitWarriors(Buildings.GetFirstBuilding(2, Buildings.BARRACKS), Settlers.BOWMAN_03, 5, 2)
+	WarriorsLib.RecruitWarriors(Buildings.GetFirstBuilding(3, Buildings.BARRACKS), Settlers.BOWMAN_03, 5, 3)
 end
-function sp3_20l3()
-	dbg.stm("SP3 30x L3 BOWMAN_03")
-	WarriorsLib.RecruitWarriors(Buildings.GetFirstBuilding(3, Buildings.BARRACKS), Settlers.BOWMAN_03, 30, 3, 1)
+function setWorkingAreaAndRecruit()
+	dbg.stm("Wir rekrutieren bei Spieler 3 nun 5 Boigenschützen und setzen den Arbeitsbereich der Kaderne um")
+	dbg.stm("WarriorsLib.SetBuildingWorkarea(buildingID, x, y, player)")
+	dbg.stm("WarriorsLib.SetBuildingWorkarea(Buildings.GetFirstBuilding(3, Buildings.BARRACKS),70, 33, 3)")
+	dbg.stm("---------")
+	WarriorsLib.SetBuildingWorkarea(Buildings.GetFirstBuilding(3, Buildings.BARRACKS),70, 33, 3)
+	WarriorsLib.RecruitWarriors(Buildings.GetFirstBuilding(3, Buildings.BARRACKS), Settlers.BOWMAN_03, 5, 3)
 end
 
 
+function addUnits(id, x, y, amount, settlertype)
+	settlertype = settlertype or Settlers.SWORDSMAN_03
+	Settlers.AddSettlers(x, y, id, settlertype, amount)	
+end
 
 
+-- gibt jede Minute einmal 1 zurueck
+function newMinute()
+	if Vars.Save8 ~= Game.Time() then
+		Vars.Save8 = Game.Time()
+		return 1
+	else
+		return 0
+	end
+end
 
+----
+--LIB fuer Minute Events---
+-----
 
 
 ----
